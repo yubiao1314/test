faf2a73e3c51e5048a026ed779d816680ba98f75
yubiao1314 <1563761182@qq.com>
2021-03-15 16:45:48 +0800

    算法上机作业

--- Use the computer/test.cpp
+++ Use the computer/test.cpp
@@ -19,76 +19,112 @@ void CreatList_L(LinkList& L, int n)
 		LinkList P = (LinkList)malloc(sizeof(LNode));
 		cin>>P->data;
 		P->next = L->next;
-		L->next=P;
+		L->next = P;
+		
 	}
 }
 void Print(LinkList L)
 {
-	cout << "打印";
+	cout << "打印 ";
 	LinkList P = L->next;
 	while (P)
 	{
-		//printf("%d ", P->data);
 		cout << " " << P->data;
 		P = P->next;
-	} 
-}
-void Insert(LinkList& L)
-{
-	int i = 0;
-	int e = 0;
-	LinkList p = L->next;
-	cout << "请输入插入位序和插入元素";
-	cin >> i>>e;
-	for (int j=0; j < i; j++)
-	{
-		p = p->next;
 	}
-	LinkList q=(LinkList)malloc(sizeof(LNode));
-	q->data = e;
-	q->next = p->next;
-	p->next = q;
+	cout << endl;
 }
-void Delate(LinkList& L)
+void Insert(LinkList& L,int n,int &e)
 {
-	int j = 1;
 	int i = 0;
-	int e = 0;
+	cout << "请输入插入位序和插入元素\n";
+	cin >> i>>e;
+	if ((i > 0) && (i <=n))
+	{
+		int j = 1;
+		LinkList p = L;
+		while (p->next && j < (i - 1))
+		{
+			p = p->next;
+			++j;
+		}
+		
+		LinkList q = (LinkList)malloc(sizeof(LNode));
+		q->data = e;
+		q->next = p->next;
+		p->next = q;
+	}
+	else cout << "输入不合法" << endl;
+	
+}
+void Delate(LinkList& L,int n,int &e)
+{
+	int i = 0;
 	cout << "请输入需要删除元素位序\n";
 	cin >> i;
-	LinkList p = L->next;
-	for (j; j <= i; j++)
+	if ((i > 0) && (i <= n))
 	{
-		p = p->next;
+		int j = 1;
+		LinkList p = L;
+		while (p->next&&j<i-1)//调整指针指向所删除元素的前一结点
+		{
+			p = p->next;
+			++j;
+		}
+		LinkList q = p->next;
+		p->next = q->next;
+		e = q->data;
+		free(q);
+		cout << "你删除的元素为:" << e << endl;
 	}
-	LinkList q = p->next;
-	e = p->data;
-	p = q->next;
-	free(q);
-	cout << "你删除的元素为:" << e << endl;
-	
+	else cout << "你的输入不合法" << endl;
+		
+}
+void Found(LinkList L,int n,int&e)
+{
+	cout << "请输入查找位序" << endl;
+	int i = 0;
+	cin >> i;
+	if ((i > 0) && (i <= n))
+	{
+		LinkList p = L;
+		int j = 1;
+		while (p->next && j <= i)
+		{
+			p = p->next;
+			++j;
+		}
+		cout << "你所查的值为：" << p->data << endl;
+	}
+	else cout << "你的输入不合法" << endl;
 }
 int main()
 {
 	LinkList L;
 	int n = 0;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
 	cout << "请输入单链表数据个数\n";
 	cin>>n;
 	CreatList_L(L, n);
 	while (1)
 	{
-		cout << "请选择你需要的操作序号:1.插入； 2.删除；3.查找；4.输出" << endl;
-		int a = 0;
+		cout << "请选择你需要的操作序号:0.退出；1.插入；2.删除；3.查找；4.输出；" << endl;
 		cin >> a;
 		switch (a)
 		{
-		case 1:Insert(L);
+		case 0:return 0;
+		case 1:Insert(L,n,d);
 			break;
-			case 2:Delate(L);
-			case 3:Found();
+		case 2:Delate(L,n,b);
+			break;
+		case 3:Found(L,n,c);
+			break;
 		case 4:Print(L);
 			break;
-		default:cout << "输入操作不合法";
+		default:cout << "输入操作不合法"<<endl;
 			break;
 		}
 	}
